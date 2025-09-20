39371d2c63167cd66a3160a1c44e9b4c28c41061
Rahul Onkar Kale <rahulonkarkale@Rahuls-MacBook-Air.local>
2025-09-20 23:02:37 +0530

    added table

--- FRONTEND/src/components/PaginatedTable.jsx
+++ FRONTEND/src/components/PaginatedTable.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
 
-export const PaginatedTable = ({dataList}) => {
+export const PaginatedTable = ({dataList, buttonText, searchPlaceholder}) => {
   
   // Pagination state
   const [currentPage, setCurrentPage] = useState(1);
@@ -14,74 +16,87 @@ export const PaginatedTable = ({dataList}) => {
   const totalPages = Math.ceil((dataList || []).length / rowsPerPage);
 
   return (
-    <div className="pt-4">
-      <div className="overflow-x-auto">
-        <table className="min-w-full border border-gray-200 rounded-lg">
-            <thead className="bg-blue-800 text-white">
-            <tr>
-                <th className="px-4 py-2 text-left">Name</th>
-                <th className="px-4 py-2 text-left">Email</th>
-                <th className="px-4 py-2 text-left">Level</th>
-                <th className="px-4 py-2 text-left">Skills</th>
-                <th className="px-4 py-2 text-left">Current Location</th>
-                <th className="px-4 py-2 text-left">Experience</th>
-                <th className="px-4 py-2 text-left">Cand Exp Upto</th>
-                <th className="px-4 py-2 text-left"></th>
-            </tr>
-            </thead>
-            <tbody>
-            {
-              currentRows && currentRows.map((row, i) => {
-                return (
-                  <tr className="odd:bg-white even:bg-gray-50" key ={i}>
-                    <td className="px-4 py-2">{row.name}</td>
-                    <td className="px-4 py-2">{row.email}</td>
-                    <td className="px-4 py-2">{row.level}</td>
-                    <td className="px-4 py-2">{row.skills}</td>
-                    <td className="px-4 py-2">{row.currentLocation}</td>
-                    <td className="px-4 py-2">{row.totalExperience}</td>
-                    <td className="px-4 py-2">{row.candidateExperienceUpto}</td>
-                </tr>
-                )
-              })
-            }
-            </tbody>
-        </table>
-      </div>  
-
-      <div className="flex justify-center mt-4 space-x-2">
-        <button
-          className="px-3 py-1 border rounded disabled:opacity-50"
-          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-          disabled={currentPage === 1}
-        >
-          Prev
-        </button>
-
-        {Array.from({ length: totalPages }, (_, i) => (
-          <button
-            key={i + 1}
-            className={`px-3 py-1 border rounded ${
-              currentPage === i + 1
-                ? "bg-blue-500 text-white"
-                : "hover:bg-gray-100"
-            }`}
-            onClick={() => setCurrentPage(i + 1)}
-          >
-            {i + 1}
-          </button>
-        ))}
-
-        <button
-          className="px-3 py-1 border rounded disabled:opacity-50"
-          onClick={() =>
-            setCurrentPage((prev) => Math.min(prev + 1, totalPages))
-          }
-          disabled={currentPage === totalPages}
-        >
-          Next
-        </button>
+    <>
+      <div className="flex justify-between items-center mb-1">
+          <h2 className="text-lg font-semibold"><input
+              type="text"
+              placeholder={searchPlaceholder}
+              className="px-3 py-2 border border-gray-300 rounded-lg w-64 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              />
+          </h2>
+          <Link to={"/panel/add"}>
+              <Button primary={true} text={buttonText}/>
+          </Link>
       </div>
+      <div className="pt-4">
+        <div className="overflow-x-auto">
+          <table className="min-w-full border border-gray-200 rounded-lg">
+              <thead className="bg-blue-800 text-white">
+              <tr>
+                  <th className="px-4 py-2 text-left">Name</th>
+                  <th className="px-4 py-2 text-left">Email</th>
+                  <th className="px-4 py-2 text-left">Level</th>
+                  <th className="px-4 py-2 text-left">Skills</th>
+                  <th className="px-4 py-2 text-left">Current Location</th>
+                  <th className="px-4 py-2 text-left">Experience</th>
+                  <th className="px-4 py-2 text-left">Cand Exp Upto</th>
+                  <th className="px-4 py-2 text-left"></th>
+              </tr>
+              </thead>
+              <tbody>
+              {
+                currentRows && currentRows.map((row, i) => {
+                  return (
+                    <tr className="odd:bg-white even:bg-gray-50" key ={i}>
+                      <td className="px-4 py-2">{row.name}</td>
+                      <td className="px-4 py-2">{row.email}</td>
+                      <td className="px-4 py-2">{row.level}</td>
+                      <td className="px-4 py-2">{row.skills}</td>
+                      <td className="px-4 py-2">{row.currentLocation}</td>
+                      <td className="px-4 py-2">{row.totalExperience}</td>
+                      <td className="px-4 py-2">{row.candidateExperienceUpto}</td>
+                  </tr>
+                  )
+                })
+              }
+              </tbody>
+          </table>
+        </div>  
+
+        <div className="flex justify-center mt-4 space-x-2">
+          <button
+            className="px-3 py-1 border rounded disabled:opacity-50"
+            onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+            disabled={currentPage === 1}
+          >
+            Prev
+          </button>
+
+          {Array.from({ length: totalPages }, (_, i) => (
+            <button
+              key={i + 1}
+              className={`px-3 py-1 border rounded ${
+                currentPage === i + 1
+                  ? "bg-blue-500 text-white"
+                  : "hover:bg-gray-100"
+              }`}
+              onClick={() => setCurrentPage(i + 1)}
+            >
+              {i + 1}
+            </button>
+          ))}
+
+          <button
+            className="px-3 py-1 border rounded disabled:opacity-50"
+            onClick={() =>
+              setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+            }
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        </div>
     </div>
+    </>
   );
 }
--- FRONTEND/src/pages/Dashboard.jsx
+++ FRONTEND/src/pages/Dashboard.jsx
@@ -24,18 +24,7 @@ export const Dashboard = () => {
             <Header />
             <main>
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                    <div className="flex justify-between items-center mb-1">
-                        <h2 className="text-lg font-semibold"><input
-                            type="text"
-                            placeholder="Search panels..."
-                            className="px-3 py-2 border border-gray-300 rounded-lg w-64 focus:outline-none focus:ring-2 focus:ring-blue-400"
-                            />
-                        </h2>
-                        <Link to={"/panel/add"}>
-                            <Button primary={true} text="Add Panel"/>
-                        </Link>
-                    </div>
-                    <PaginatedTable dataList={panels}/>
+                    <PaginatedTable dataList={panels} buttonText={"Add Panel"} searchPlaceholder={"Search Panels..."}/>
                 </div>
             </main>
         </>
