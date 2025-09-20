535b2f4fc8c79cc43ab3b6f4fb0db114aa9fcf76
Rahul Onkar Kale <rahulonkarkale@Rahuls-MacBook-Air.local>
2025-09-20 20:32:26 +0530

    added frontend

---
+++ FRONTEND/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+import { Header } from "../components/Header";
+
+export const Dashboard = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                        Dashboard
+                </div>
+            </main>
+            
+
+        </>
+    );
+}
